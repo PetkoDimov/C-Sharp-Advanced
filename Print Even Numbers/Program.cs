ce03716c4a049d41b7d05ae35925fb1c04038b75
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 19:57:06 +0300

    Add files via upload

---
+++ Print Even Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Print_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> numbers = new Queue<int>(input);
+            Queue<int> result = new Queue<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int currentNumber = numbers.Peek();
+
+                if (currentNumber % 2 != 0)
+                {
+                    numbers.Dequeue();
+                }
+                else
+                {
+                    result.Enqueue(numbers.Dequeue());
+                }
+
+            }
+
+            Console.WriteLine(string.Join(", " , result));
+            
+        }
+    }
+}
---
+++ Songs/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Songs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(", ").ToArray();
+            Queue<string> songs = new Queue<string>(input);
+
+            input = Console.ReadLine().Split(" ").ToArray();
+
+            while (songs.Any())
+            {
+                string command = input[0];
+
+                if (command == "Play")
+                {
+                    songs.Dequeue();
+                }
+                else if (command == "Add")
+                {
+
+                    string nameOfSong = string.Empty;
+                    string[] temp = new string[input.Length - 1];
+
+                    for (int i = 1; i < input.Length; i++)
+                    {
+                        temp[i - 1] = input[i];
+                    }
+                    nameOfSong = string.Join(" ", temp);
+
+                    if (songs.Contains(nameOfSong))
+                    {
+                        Console.WriteLine($"{nameOfSong} is already contained!");
+                    }
+                    else
+                    {
+                        songs.Enqueue(nameOfSong);
+                    }
+
+                }
+                else if (command == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", songs));
+                }
+
+                if (!songs.Any())
+                {
+                    Console.WriteLine("No more songs!");
+                    break;
+                }
+
+                input = Console.ReadLine().Split(" ").ToArray();
+
+            }
+
+        }
+    }
+}
