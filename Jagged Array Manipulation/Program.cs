cb0663b8a5bbc091d82defd9a0e7d6268647a840
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 19:17:23 +0300

    Add files via upload

---
+++ Jagged Array Manipulation/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+namespace Jagged_Array_Manipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int[][] jaggedMatrix = new int[n][];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] rowData = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                jaggedMatrix[row] = rowData;
+            }
+
+            for (int row = 0; row < n - 1; row++)
+            {
+                int[] firstArr = jaggedMatrix[row];
+                int[] secondArr = jaggedMatrix[row + 1];
+                if (firstArr.Length == secondArr.Length)
+                {
+                    jaggedMatrix[row] = firstArr.Select(e => e * 2).ToArray();
+                    jaggedMatrix[row + 1] = secondArr.Select(e => e * 2).ToArray();
+                }
+                else
+                {
+
+                    jaggedMatrix[row] = firstArr.Select(e => e / 2).ToArray();
+                    jaggedMatrix[row + 1] = secondArr.Select(e => e / 2).ToArray();
+                }
+            }
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                string[] commandData = command.Split(" ");
+                int rowIndex = int.Parse(commandData[1]);
+                int colIndex = int.Parse(commandData[2]);
+                int value = int.Parse(commandData[3]);
+
+                bool isValidCell = rowIndex >= 0 && rowIndex < n && colIndex >= 0 && colIndex < jaggedMatrix[rowIndex].Length;
+
+                if (!isValidCell)
+                {
+                    command = Console.ReadLine();
+                    continue;
+                }
+
+                if (commandData[0] == "Add")
+                {
+                    jaggedMatrix[rowIndex][colIndex] += value;
+                }
+                else if (commandData[0] == "Subtract")
+                {
+                    jaggedMatrix[rowIndex][colIndex] -= value;
+                }
+
+                command = Console.ReadLine();
+            }
+
+            for (int row = 0; row < n; row++)
+            {
+                Console.WriteLine(string.Join(" ", jaggedMatrix[row]));
+            }
+
+        }
+    }
+}
