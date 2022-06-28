99ef2c2e80252b34e83c594229f17a37cf3d3dc2
Petko Dimov <108327041+PetkoDimov@users.noreply.github.com>
2022-06-28 20:01:13 +0300

    Add files via upload

---
+++ Stack Sum/Hot Potato/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] children = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<string> potatoQueue = new Queue<string>();
+
+            while (true)
+            {
+                potatoQueue.Enqueue(potatoQueue.Dequeue());
+            }
+        }
+    }
+}
---
+++ Super Market/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Super_Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<string> clients = new Queue<string>();
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    foreach (var item in clients)
+                    {
+                        Console.WriteLine(item);
+                    }
+
+                    clients.Clear();
+                }
+                else
+                {
+                    clients.Enqueue(input);
+                }
+
+
+                input = Console.ReadLine();
+            }
+
+            if (input == "End")
+            {
+                Console.WriteLine($"{clients.Count} people remaining.");
+            }
+
+        }
+    }
+}
